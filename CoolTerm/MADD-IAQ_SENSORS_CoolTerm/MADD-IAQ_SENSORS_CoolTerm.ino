fd85d0408bd3f5d7f90edc4b08b70c045e74763c
mconangla <161712424+mconangla@users.noreply.github.com>
2025-01-13 22:44:23 +0100

    Update MADD-IAQ_SENSORS_CoolTerm.ino

--- CoolTerm/MADD-IAQ_SENSORS_CoolTerm/MADD-IAQ_SENSORS_CoolTerm.ino
+++ CoolTerm/MADD-IAQ_SENSORS_CoolTerm/MADD-IAQ_SENSORS_CoolTerm.ino
@@ -10,7 +10,7 @@ unsigned long interval = 10000;
 ///// PM25 Libraries ///// - Adafruit PM25 AQI
 #include "Adafruit_PM25AQI.h"
 #include <SoftwareSerial.h>
-SoftwareSerial pmSerial(12, 14);
+SoftwareSerial pmSerial(14, 12);
 Adafruit_PM25AQI aqi = Adafruit_PM25AQI();
 PM25_AQI_Data data;
 
