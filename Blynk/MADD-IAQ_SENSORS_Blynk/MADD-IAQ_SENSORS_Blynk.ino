13cf169cee40cc42ed7523a5a1d38415be2bf82f
marccobe <161712424+mconangla@users.noreply.github.com>
2025-01-19 11:29:24 +0100

    Update MADD-IAQ_SENSORS_Blynk.ino

--- Blynk/MADD-IAQ_SENSORS_Blynk/MADD-IAQ_SENSORS_Blynk.ino
+++ Blynk/MADD-IAQ_SENSORS_Blynk/MADD-IAQ_SENSORS_Blynk.ino
@@ -16,22 +16,24 @@ PM25_AQI_Data data;
 SCD4x mySensor;
 
 ///// Blynk libraries /////
-/* Fill in information from Blynk Device Info here */
+/* Fill-in information from Blynk Device Info here */
 #define BLYNK_TEMPLATE_ID           "TMPxxxxxx"
 #define BLYNK_TEMPLATE_NAME         "Device"
 #define BLYNK_AUTH_TOKEN            "YourAuthToken"
 
 /* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
 #include <Blynk.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-#define BLYNK_PRINT Serial
-BlynkTimer timer;
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "YOURWIFINAME";
-char pass[] = "YOURWIFIPASSWORD";
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+
+BlynkTimer timer;
 
 void setup() {
   Serial.begin(115200);
@@ -40,11 +42,11 @@ void setup() {
 
   ////// Blynk Setup //////
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  timer.setInterval(30000L, myTimer);  //timer will run every 30 sec
+  timer.setInterval(30000L, myTimer);  // Timer runs every 30 seconds
 
   ////// PM25 Setup //////
   pmSerial.begin(9600);
-  if (!aqi.begin_UART(&pmSerial)) {  // connect to the sensor over software serial
+  if (!aqi.begin_UART(&pmSerial)) {  // Connect to the sensor over software serial
     Serial.println("Could not find PM 2.5 sensor!");
     while (1) delay(10);
   }
@@ -55,40 +57,18 @@ void setup() {
     Serial.println(F("SCD41 Sensor not detected."));
     while (1)
       ;
-  }  
+  }
 }
 
 void loop() {
   Blynk.run();
-  timer.run();
+  timer.run();  // Initiates BlynkTimer
 }
 
 void myTimer() {
-  readPM25();
   readSCD41();
+  readPM25();
   writeBlynk();
-}
-
-void readPM25() {
-  ////// PM25 Reading //////
-  while (!aqi.read(&data)) {
-    if (Monitor_log) {
-      Serial.println("Waiting PM25 sensor data...");
-    }
-    delay(500);
-  }
-  if (Monitor_PM25) {
-    Serial.print(F("PM1.0:"));
-    Serial.print(data.pm10_standard);
-    Serial.print("µg/m3\t");
-    Serial.print(F("PM2.5:"));
-    Serial.print(data.pm25_standard);
-    Serial.print("µg/m3\t");
-    Serial.print(F("PM10:"));
-    Serial.print(data.pm100_standard);
-    Serial.print("µg/m3");
-    Serial.println();
-  }
 }
 
 void readSCD41() {
@@ -100,22 +80,44 @@ void readSCD41() {
     delay(500);
   }
   if (Monitor_SCD41) {
-    Serial.print(F("Temperature:"));
+    Serial.print(F("Temperature: "));
     Serial.print(mySensor.getTemperature(), 1);
-    Serial.print("ºC\t");
-    Serial.print(F("Humidity:"));
+    Serial.print(" °C\t");
+    Serial.print(F("Humidity: "));
     Serial.print(mySensor.getHumidity(), 1);
-    Serial.print("%RH\t");
-    Serial.print(F("CO2:"));
+    Serial.print(" %RH\t");
+    Serial.print(F("CO2: "));
     Serial.print(mySensor.getCO2());
-    Serial.print("ppm");
+    Serial.print(" ppm");
+    Serial.println();
+  }
+}
+
+void readPM25() {
+  ////// PM25 Reading //////
+  while (!aqi.read(&data)) {
+    if (Monitor_log) {
+      Serial.println("Waiting PM25 sensor data...");
+    }
+    delay(500);
+  }
+  if (Monitor_PM25) {
+    Serial.print(F("PM1.0: "));
+    Serial.print(data.pm10_standard);
+    Serial.print(" µg/m3\t");
+    Serial.print(F("PM2.5: "));
+    Serial.print(data.pm25_standard);
+    Serial.print(" µg/m3\t");
+    Serial.print(F("PM10: "));
+    Serial.print(data.pm100_standard);
+    Serial.print(" µg/m3");
     Serial.println();
   }
 }
 
 void writeBlynk() {
-  //// Blynk Writting /////
-  // set the fields with the values
+  //// Blynk Writing /////
+  // Set the fields with the values
   Blynk.virtualWrite(V1, mySensor.getTemperature());
   Blynk.virtualWrite(V2, mySensor.getHumidity());
   Blynk.virtualWrite(V3, mySensor.getCO2());
