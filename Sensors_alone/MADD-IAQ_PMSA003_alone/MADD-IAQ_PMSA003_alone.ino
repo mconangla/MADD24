2753e3588b9c21060883a17938d3fc54ded5b54b
marccobe <161712424+mconangla@users.noreply.github.com>
2025-01-28 16:06:18 +0100

    added delay

--- Sensors_alone/MADD-IAQ_PMSA003_alone/MADD-IAQ_PMSA003_alone.ino
+++ Sensors_alone/MADD-IAQ_PMSA003_alone/MADD-IAQ_PMSA003_alone.ino
@@ -22,7 +22,7 @@ void setup() {
   Serial.println("Adafruit PMSA003I Air Quality Sensor");
 
   // Wait one second for sensor to boot up!
-  delay(1000);
+  delay(3000);
 
   // If using serial, initialize it and set baudrate before starting!
   // Uncomment one of the following
